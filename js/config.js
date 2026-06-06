c0eb1a8cf53ccc66e0f9e27078e6ec02599c7e4f
iledgerlpg <iledgerlpg@gmail.com>
2026-06-06 15:12:16 +0700

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,3 +1,8 @@
-// 
+// js/config.js
+
 window.APP_URL = 'https://script.google.com/macros/s/AKfycbyUj-_hBNw2LFuWjxSf0fxrp85Q67qIBryWG-FFsBYZWNxYbI3-AC084jVTjKmDtPL5/exec';
-window.CURRENT_PAGE = 'dashboard';
+
+// Deteksi nama halaman otomatis dari URL (misal: "dashboard.html" -> "dashboard")
+const path = window.location.pathname;
+const page = path.split("/").pop().replace(".html", "");
+window.CURRENT_PAGE = page || "index"; // default ke index kalau kosong
